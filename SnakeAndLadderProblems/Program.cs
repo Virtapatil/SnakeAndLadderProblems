ffb259da3f85cfe2f21c6881b4032409448d1e12
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-28 21:36:04 +0530

    [Virta] Modifing Number Reapeat till the player reaches the winning position 100

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using System.Numerics;
+using System.Xml.Linq;
 
 namespace SnakeAndLadderProblems
 {
@@ -11,38 +13,37 @@ namespace SnakeAndLadderProblems
             int startPosition = 0;
             Console.WriteLine("the player start position is:" + startPosition);
             Random random = new Random();
-            int dieValue = random.Next(1, 7);
-            int dieCount = random.Next(1, 7);
-            Console.WriteLine("The player roll a die:" + dieValue );
-            const int noPlay = 1, ladder = 2, snake = 3;
-            int option = random.Next(1, 4);
-            Console.WriteLine("option:" + option);
-
-            switch (option)
+            while (startPosition < 100)
             {
-                case noPlay:
-                    Console.WriteLine("The player stays in the same position:" + startPosition);
-                    break;
-                case ladder:
-                    startPosition = startPosition + dieValue;
-                    if (startPosition > 100)
-                    {
+                int dieValue = random.Next(1, 7);
+                Console.WriteLine("The player roll a die:" + dieValue);
+                const int noPlay = 1, ladder = 2, snake = 3;
+                int option = random.Next(1, 4);
+                Console.WriteLine("option:" + option);
+
+                switch (option)
+                {
+                    case noPlay:
+                        Console.WriteLine("The player stays in the same position:" + startPosition);
+                        break;
+                    case ladder:
+                        startPosition = startPosition + dieValue;
+                        if (startPosition > 100)
+                        {
+                            startPosition = startPosition - dieValue;
+                        }
+                        Console.WriteLine("player got ladder and move ahead :" + startPosition);
+                        break;
+                    case snake:
                         startPosition = startPosition - dieValue;
-                    }
-                    Console.WriteLine("player got ladder and move ahead :" + startPosition);
-                    dieCount++;
-                    break;
-                case snake:
-                    startPosition = startPosition - dieValue;
-                    if (startPosition < 0)
-                    {
-                        startPosition = 0;
-                    }
-                    Console.WriteLine("player got snake and move behind :" + startPosition);
-                    dieCount++;
-                    break;
+                        if (startPosition < 0)
+                        {
+                            startPosition = 0;
+                        }
+                        Console.WriteLine("player got snake and move behind :" + startPosition);
+                        break;
+                }
             }
         }
-        
     }
 }
