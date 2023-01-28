0e05e95dfccac03ee332fb1be4b8f76854b7edbd
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-28 20:21:25 +0530

    [Virta] Modifing Player Rolls The Die between 0 to 6

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -7,8 +7,11 @@ namespace SnakeAndLadderProblems
         public static void Main(string[] args)
 
         { 
-             int startPosition = 0;
-             Console.WriteLine("the player start position is:" + startPosition);
+            int startPosition = 0;
+            Console.WriteLine("the player start position is:" + startPosition);
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            Console.WriteLine("The player roll a die:" + dieValue);
         }
         
     }
