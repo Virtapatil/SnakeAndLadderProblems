366ff86088e2701847b81f7261f3fb62c280c609
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-28 20:56:48 +0530

    [Virta] Modifing Player Then Checks for a Option

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace SnakeAndLadderProblems
 {
@@ -7,8 +8,40 @@ namespace SnakeAndLadderProblems
         public static void Main(string[] args)
 
         { 
-             int startPosition = 0;
-             Console.WriteLine("the player start position is:" + startPosition);
+            int startPosition = 0;
+            Console.WriteLine("the player start position is:" + startPosition);
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            int dieCount = random.Next(1, 7);
+            Console.WriteLine("The player roll a die:" + dieValue );
+            const int noPlay = 1, ladder = 2, snake = 3;
+            int option = random.Next(1, 4);
+            Console.WriteLine("option:" + option);
+
+            switch (option)
+            {
+                case noPlay:
+                    Console.WriteLine("The player stays in the same position:" + startPosition);
+                    break;
+                case ladder:
+                    startPosition = startPosition + dieValue;
+                    if (startPosition > 100)
+                    {
+                        startPosition = startPosition - dieValue;
+                    }
+                    Console.WriteLine("player got ladder and move ahead :" + startPosition);
+                    dieCount++;
+                    break;
+                case snake:
+                    startPosition = startPosition - dieValue;
+                    if (startPosition < 0)
+                    {
+                        startPosition = 0;
+                    }
+                    Console.WriteLine("player got snake and move behind :" + startPosition);
+                    dieCount++;
+                    break;
+            }
         }
         
     }
