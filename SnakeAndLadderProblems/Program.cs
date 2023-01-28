6a9bea863b85c57d8a5fe03d27ef6d59274ac553
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-28 22:36:53 +0530

    [Virta] Modifing Playing The Game With 2 Player

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -6,50 +6,119 @@ using System.Xml.Linq;
 namespace SnakeAndLadderProblems
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            int startPosition = 0, dieCount = 0;
-            const int noPlay = 1, ladder = 2, snake = 3;
-            Console.WriteLine("welcome to snake and ladder program");
+            const int snake = 0;
+            const int ladder = 1;
+            const int NoGame = 2;
+            const int player1 = 0;
+            const int player2 = 1;
+            int diceCount = 0;
+            int playerPosition1 = 0;
+            int playerPosition2 = 0;
 
-            while (startPosition < 100)
+            int player = player1;
+            Random random = new Random();
+
+            while (playerPosition1 < 100 && playerPosition2 < 100)
             {
-                Random random = new Random();
-                int dieValue = random.Next(1, 7);
-                Console.WriteLine("The player roll a die:" + dieValue);
-
-                int option = random.Next(1, 4);
-                Console.WriteLine("option:" + option);
-
-                switch (option)
+                int diceRoll, choice;
+                diceRoll = random.Next(1, 7);
+                Console.WriteLine("Dice Roll= " + diceRoll);
+                choice = random.Next(0, 3);
+                if (player == player1)
                 {
-                    case noPlay:
-                        Console.WriteLine("The player stays in the same position:" + startPosition);
-                        break;
-                    case ladder:
-                        startPosition = startPosition + dieValue;
-                        if (startPosition > 100)
-                        {
-                            startPosition = startPosition - dieValue;
-                        }
-                        Console.WriteLine("player got ladder and move ahead :" + startPosition);
-                        dieCount++;
-                        break;
-                    case snake:
-                        startPosition = startPosition - dieValue;
-                        if (startPosition < 0)
-                        {
-                            startPosition = 0;
-                        }
-                        Console.WriteLine("player got snake and move behind :" + startPosition);
-                        dieCount++;
-                        break;
+                    switch (choice)
+                    {
+                        case snake:
+                            playerPosition1 -= diceRoll;
+                            Console.WriteLine("Snake");
+                            if (playerPosition1 < 0)
+                            {
+                                playerPosition1 = 0;
+                                Console.WriteLine("Restart Game");
+                                Console.WriteLine("Player 1 position : " + playerPosition1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player 1 position : " + playerPosition1);
+                            }
+                            player = player2;
+                            break;
+                        case ladder:
+                            playerPosition1 += diceRoll;
+                            Console.WriteLine("Ladder");
+                            if (playerPosition1 == 100)
+                            {
+                                Console.WriteLine(" PLAYER-1 Wins");
+                            }
+                            else if (playerPosition1 > 100)
+                            {
+                                playerPosition1 -= diceRoll;
+                                Console.WriteLine("Player 1 Position is above 100; doesn't Count " + playerPosition1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player 1 position : " + playerPosition1);
+                            }
+                            player = player1;
+                            break;
+                        case NoGame:
+                            Console.WriteLine("No Game");
+                            Console.WriteLine("Player 1 position : " + playerPosition1);
+                            player = player2;
+                            break;
+                    }
                 }
-                Console.WriteLine("player is at:" + startPosition);
+                else
+                {
+                    switch (choice)
+                    {
+                        case snake:
+                            playerPosition2 -= diceRoll;
+                            Console.WriteLine("Snake");
+                            if (playerPosition2 < 0)
+                            {
+                                playerPosition2 = 0;
+                                Console.WriteLine("Restart Game");
+                                Console.WriteLine("Player 2 position : " + playerPosition2);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player 2 position : " + playerPosition2);
+                            }
+                            player = player1;
+                            break;
+                        case ladder:
+                            playerPosition2 += diceRoll;
+                            Console.WriteLine("Ladder");
+                            if (playerPosition2 == 100)
+                            {
+                                Console.WriteLine(" PLAYER-2 Wins");
+                            }
+                            else if (playerPosition2 > 100)
+                            {
+                                playerPosition2 -= diceRoll;
+                                Console.WriteLine("Player 2 Position is above 100; doesn't Count " + playerPosition2);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player 2 position : " + playerPosition2);
+                            }
+                            player = player2;
+                            break;
+                        case NoGame:
+                            Console.WriteLine("No Game");
+                            Console.WriteLine("Player 2 position : " + playerPosition2);
+                            player = player1;
+                            break;
+                    }
+                }
+                diceCount++;
             }
-            Console.WriteLine("Dice count:" + dieCount);
-            Console.ReadLine();
+            Console.WriteLine("Dice Count to Win the Game: " + diceCount);
         }
     }
 }
